62a62a08db987967b0beb170b2e69c1b250154f1
minhajul islam <mm01882390860@gmail.com>
2026-01-27 20:57:39 +0600

    mouseDot practice task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MovingDot from './components/MovingDot/MovingDot'
+import MouseDot from './components/MouseDot/MouseDot'
 
 function App() {
   return (
     <div>
-      <MovingDot />
+      {/* <MovingDot /> */}
+      <MouseDot />
     </div>
   )
 }
--- src/components/MouseDot/MouseDot.jsx
+++ src/components/MouseDot/MouseDot.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MouseDot() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleDot = (e) => {
+    setPosition({
+      x: e.clientX,
+      y: e.clientY
+    })
+  }
   return (
-    <div>
-      
+    <div
+      onClick={handleDot}
+      style={{
+        width: '100vw',
+        height: '100vh',
+        position: 'relative'
+      }}
+    >
+      <div style={{
+        width: 20,
+        height: 20,
+        background: 'red',
+        borderRadius: '50%',
+        position: 'absolute',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        right: -10
+      }} />
     </div>
   )
 }
